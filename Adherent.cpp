ad07cf61f0d6985755aebfc1470229edb9acf417
GhadaAmakrane <ghada.amakrane1@gmail.com>
2024-01-02 17:43:12 +0100

    adh

--- Adherent.cpp
+++ Adherent.cpp
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+#include "Livre.h"
 #include "Adherent.h"
 #include <iostream>
 using namespace std;
@@ -106,71 +106,5 @@ bool Adherent::operator%(string ISBN)  {
 
 
 
-=======
-#include "Adherent.h"
-#include <iostream>
-using namespace std;
 
-// Constructeur par défaut
-Adherent::Adherent() {
-    nom = "";
-    prenom = "";
-    adresse = "";
-    numeroAdherent = 0;
-    capaciteMaxLivresEmpruntes = 10; 
-    livresEmpruntes = new Livre*[capaciteMaxLivresEmpruntes];
-    nombreLivresEmpruntes = 0;
-}
 
-// Constructeur prenant les attributs spécifiques
-Adherent::Adherent(string nom, string prenom, string adresse) {
-    this->nom = nom;
-    this->prenom = prenom;
-    this->adresse = adresse;
-    numeroAdherent = 0; 
-    capaciteMaxLivresEmpruntes = 10; 
-    livresEmpruntes = new Livre*[capaciteMaxLivresEmpruntes];
-    nombreLivresEmpruntes = 0;
-}
-
-// Destructeur
-Adherent::~Adherent() {
-    delete[] livresEmpruntes; 
-    
-}
-
-// Getters et setters
-string Adherent::getNom(){
-    return nom;
-}
-
-void Adherent::setNom(string nom) {
-    this->nom = nom;
-}
-
-string Adherent::getPrenom() {
-    return prenom;
-}
-
-void Adherent::setPrenom(string prenom) {
-    this->prenom = prenom;
-}
-
-string Adherent::getAdresse()  {
-    return adresse;
-}
-
-void Adherent::setAdresse(string adresse) {
-    this->adresse = adresse;
-}
-
-// Méthodes supplémentaires pour gérer les opérations de l'adhérent, comme emprunter et rendre des livres
-void Adherent::emprunterLivre(Livre* livre) {
-    if (nombreLivresEmpruntes < capaciteMaxLivresEmpruntes) {
-        livresEmpruntes[nombreLivresEmpruntes] = livre;
-        nombreLivresEmpruntes++;
-    } else {
-        // Gérer le dépassement de la capacité maximale des livres empruntés
-    }
-}
->>>>>>> 1256a6fe95eb15d31e76b688012d6108367311a8
--- Adherent.h
+++ Adherent.h
@@ -1,57 +1,3 @@
-<<<<<<< HEAD
-#ifndef __ADHERANT_H__
-#define __ADHERANT_H__
-
-#include <iostream>
-using namespace std;
-#include "Livre.h"
-#include "Bibliotheque.h"
-#include "Personne.h"
-
-class Adherent : public Personne  {
-private:
-    string nom;
-    string prenom;
-    string adresse;
-    int numeroAdherent;
-    Bibliotheque bibliothequeInscrite;
-    Livre **livresEmpruntes;   // Tableau de pointeurs de Livre
-    int nombreLivresEmpruntes; // Nombre de livres empruntés
-    int capaciteMaxLivresEmpruntes;
-
-public: 
-
-    Adherent();
-    Adherent(string,string,string);
-    ~Adherent();
-        
-    //getters && setters
-    string getNom();
-    void setNom(string);
-    string getPrenom();
-    void setPrenom(string );
-    string getAdresse();
-    void setAdresse(string );
-    int getNumeroAdherent();
-    void setNumeroAdherent(int);
-    Bibliotheque getBibliothequeInscrite();
-    void setBibliothequeInscrite(Bibliotheque bibliothequeInscrite);
-    int getCapaciteMaxLivresEmpruntes();
-    void setCapaciteMaxLivresEmpruntes(int capaciteMaxLivresEmpruntes);
-
-    //Methodes 
-    void emprunterLivre(Livre*);
-    void rendreLivre(string);
-
-    //Fct amie
-    friend void afficherInfosAdherent(const Adherent& adherent);
-
-    //Operateur
-    bool operator%(string);
-    
-
-};
-=======
 #ifndef __ADHERANT_H__
 #define __ADHERANT_H__
 
@@ -94,8 +40,14 @@ public:
     //Methodes 
     void emprunterLivre(Livre*);
     void rendreLivre(string);
+
+    //Fct amie
+    friend void afficherInfosAdherent(const Adherent& adherent);
+
+    //Operateur
+    bool operator%(string);
     
 
 };
->>>>>>> 1256a6fe95eb15d31e76b688012d6108367311a8
+
 #endif // __ADHERANT_H__
